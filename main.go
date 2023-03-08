accde99d5773aa6fc3c8b1cd66a794153b1f26f3
Snowchord <Cleveland225@163.com>
2023-03-08 15:06:07 +0800

    前缀树路由，可以识别动态路由

--- main.go
+++ main.go
@@ -16,6 +16,10 @@ func helloHandler(c *gee.Context) {
 	c.String(http.StatusOK, "hello %s, you are at %s\n",c.Query("name"), c.Path)
 }
 
+func helloParam(c *gee.Context) {
+	c.String(http.StatusOK, "hello %s, you are at %s\n",c.Param("name"), c.Path)
+}
+
 func Form(c *gee.Context){
 	c.JSON(http.StatusOK, gee.H{
 		"username":c.PostForm("username"),
@@ -27,8 +31,11 @@ func main(){
 	r:=gee.New()
 	r.GET("/",indexHandler)
 	r.GET("/hello",helloHandler)
-	r.POST("/login",Form)
-
+	// r.POST("/login",Form)
+	r.GET("/hello/:name",helloParam)
+	r.GET("/assets/*filepath",func(c *gee.Context){
+		c.JSON(http.StatusOK, gee.H{"filepath": c.Param("filepath")})
+	})
 	r.Run(":8989")
 	// http.HandleFunc("/",indexHandler)
 	// http.HandleFunc("/hello",helloHandler)
