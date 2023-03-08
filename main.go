c87e4903d7f27696436baa15dd3b4f118e609299
Snowchord <Cleveland225@163.com>
2023-03-08 15:18:01 +0800

    分组管理

--- main.go
+++ main.go
@@ -29,13 +29,45 @@ func Form(c *gee.Context){
 
 func main(){
 	r:=gee.New()
-	r.GET("/",indexHandler)
-	r.GET("/hello",helloHandler)
-	// r.POST("/login",Form)
-	r.GET("/hello/:name",helloParam)
-	r.GET("/assets/*filepath",func(c *gee.Context){
-		c.JSON(http.StatusOK, gee.H{"filepath": c.Param("filepath")})
+	// r.GET("/",indexHandler)
+	// r.GET("/hello",helloHandler)
+	// // r.POST("/login",Form)
+	// r.GET("/hello/:name",helloParam)
+	// r.GET("/assets/*filepath",func(c *gee.Context){
+	// 	c.JSON(http.StatusOK, gee.H{"filepath": c.Param("filepath")})
+	// })
+
+	r.GET("/index", func(c *gee.Context) {
+		c.HTML(http.StatusOK, "<h1>Index Page</h1>")
 	})
+	v1 := r.Group("/v1")
+	{
+		v1.GET("/", func(c *gee.Context) {
+			c.HTML(http.StatusOK, "<h1>Hello Gee</h1>")
+		})
+
+		v1.GET("/hello", func(c *gee.Context) {
+			// expect /hello?name=geektutu
+			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
+		})
+	}
+	v2 := r.Group("/v2")
+	{
+		v2.GET("/hello/:name", func(c *gee.Context) {
+			// expect /hello/geektutu
+			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
+		})
+		v2.POST("/login", func(c *gee.Context) {
+			c.JSON(http.StatusOK, gee.H{
+				"username": c.PostForm("username"),
+				"password": c.PostForm("password"),
+			})
+		})
+
+	}
+
+
+	
 	r.Run(":8989")
 	// http.HandleFunc("/",indexHandler)
 	// http.HandleFunc("/hello",helloHandler)
